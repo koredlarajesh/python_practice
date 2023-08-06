edef7fc8f32b5c2dffe71ea4a8ff69994df7ac83
Rajesh koredla <rajesh.koredla25@gmail.com>
2023-08-06 16:32:53 +0530

    Add files via upload

---
+++ python_practicse.py
@@ -0,0 +1,125 @@
+1+1
+
+str = "a"+"b"
+print(str)
+
+import this
+
+print("this is my first print statement")
+
+### variables
+g=True
+print(type(g))
+
+*a=67
+print(type(*a))
+
+_a = 45
+print(type(_a))
+
+a="kore"
+b="dla"
+print(a+b)
+
+a="kored"
+b=4
+print(a+b)
+
+a="kored"
+b="4"
+print(a+b)
+
+True + True
+
+True + False
+
+input() # by default whatever u gave input it will take as a string
+
+a=input()
+
+type(a)
+
+print(a)
+
+b=12
+
+print(a+b)  # it will give error
+# TypeError: can only concatenate str (not "int") to str
+
+# string
+s="rajesh"
+print(s)
+print(s[0])
+print(s[1:3])
+print(s[:])
+print(s[::-1])
+print(s[3:1:-1])
+print(s*3)
+print(s.split("a"))
+r=s*3
+print(r.split("a"))
+print(r.capitalize())
+
+a="koredla"
+b="rajesh"
+print(a.join(b))
+
+" ".join(b)
+
+a="ram sita ram"
+b="koredla"
+print(a.join(b))
+print(b.join(a))
+
+"".join(a)
+
+" ".join(a)
+
+print(a.isalnum())
+print(a.isascii())
+print(a.isupper())
+
+a=1212
+print(a.isdigit())
+
+a="1212"
+print(a.isdigit())
+
+a.encode()
+
+# list
+
+l=[1,2,3,4,455,6]
+l.append(6)
+print(l)
+
+l.pop()
+print(l)
+
+l.pop(2)
+print(l)
+
+l.insert(2,"ramya")
+print(l)
+
+l=[1,2,3,4,5,6,7,7.7,777,7,"ram"]
+l.reverse()
+
+list("sudh")
+
+l1=['rajesh']
+l2=['ram']
+print(l1+l2)
+
+
+
+
+
+
+
+
+
+
+
+
+
